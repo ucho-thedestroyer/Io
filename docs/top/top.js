874197ee4b21d6d28a5379556e963595a8cdb23b
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-04-09 18:56:00 +0800

    Update top.js

--- docs/top/top.js
+++ docs/top/top.js
@@ -149,13 +149,3 @@ swiper.on("slideChange", () => {
 });
 
 
-function getVote(int) {
-  var xmlhttp=new XMLHttpRequest();
-  xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-      document.getElementById("poll").innerHTML=this.responseText;
-    }
-  }
-  xmlhttp.open("GET","https://github.com/ucho-thedestroyer/Io/raw/Backup/docs/top/poll_vote.php?vote="+int,true);
-  xmlhttp.send();
-}
