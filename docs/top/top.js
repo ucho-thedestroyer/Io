2f402e9e7f5d3642c0ef830dfd4c3cf16a9645fb
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-04-09 16:49:53 +0800

    Update top.js

--- docs/top/top.js
+++ docs/top/top.js
@@ -147,3 +147,15 @@ swiper.on("slideChange", () => {
   updateSongInfo();
   playPause();
 });
+
+
+function getVote(int) {
+  var xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange=function() {
+    if (this.readyState==4 && this.status==200) {
+      document.getElementById("poll").innerHTML=this.responseText;
+    }
+  }
+  xmlhttp.open("GET","poll_vote.php?vote="+int,true);
+  xmlhttp.send();
+}
