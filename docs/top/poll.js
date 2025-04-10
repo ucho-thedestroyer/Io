aa4d89d3624c0b9fb62e58f85d0d2158c0367c9b
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-04-10 12:21:51 +0800

    Update poll.js

--- docs/top/poll.js
+++ docs/top/poll.js
@@ -1,52 +1,78 @@
-const options = [
-  {id:"option1",text: "JavaScript", votes:0}, 
-  {id:"option2", text:"Python", votes:0}, 
-  {id: "option3", text: "Java", votes:0},
-  {id:"option4", text:"(++", votes:0},
+let poll =
+{
+    question: "Which is your favorite programming language?",
+    answers:["Python", "Java", "R", "Php"],
+    pollcount:100,
+    answerweight:[60,10,20,10], //sum = 100
+    selectanswer:-1
+};
 
-  function submitVote(){
-const selectedOption = document.querySelector('input[name="poll"]:checked');
-// console. log(selectedOption.value);
-if(!selectedOption){
-  
-alert("Please select an optin.");
-return;
+let polldom =
+{
+    question:document.querySelector(".poll .question"),
+    answers:document.querySelector(".poll .answers")
+};
+
+polldom.question.innerText = poll.question;
+polldom.answers.innerHTML  = poll.answers.map(function(answer,i){
+    return(
+        `
+        <div class="answer" onclick="markanswer('${i}')">
+        ${answer}
+        <span class="percentage_bar"></span>
+        <span class="percentage_value"></span>
+        </div>
+        
+        `
+    );
+}).join("");
+
+
+function markanswer(i)
+{
+    poll.selectanswer = +i;
+
+    try{
+        document.querySelector(".poll .answers .answer.selected")
+        .classList.remove(".selected");
+    }
+    catch(msg){}
+
+    document.querySelectorAll(".poll .answers .answer")
+    [+i].classList.add(".selected");
+
+
+    showresults();
 }
 
-const optionId = selectedOption.value;
-const selectedOptionObj = options.find((option)=> option.id === optionid);
-// console. log(selectedOptionObj):
-if(selectedOptionObj){
-  selectedOptionObj.votes++;
-  console.log(selectedOptionObj);
-  displayResult();
-  
-function displayResult(){
-const result = document.getElementById('result');
-result.innerHTML = "";
 
-options.forEach((option)=>{
-  const percentage = ((option.votes/ getTotalVotes()) * 100).toFixed(2) || 0;
-  const barWidth = percentage > 0 ? percentage + "%" : "0%";
-  
-  const optionResult = document.createElement("div");
-  optionResult.className = "option-result";
-  optionResult.innerHTML = '
-  <span class = "option-text">${option.text}</span>
-  <div class = "bar-container">
-  <div class = "bar" style="width: ${barWidth};"></div>
-  </div>
-  <span class = "percentage">${percentage}%</span>
-  ;
-  result.appendChild(optionResult);
-});
 
-function getTotalVotes(){
-  return options.reduce((total,option)=> total + option.votes,0);
+function showresults()
+{
+    let answers = document.querySelectorAll(".poll .answers .answer");
+
+    for (let i = 0; i < answers.length; i++) {
+        
+        let percentage = 0;
+
+        if (i==poll.selectanswer) 
+        {
+            percentage = Math.round
+            (
+                (poll.answerweight[i] + 1) * 100 / (poll.pollcount + 1)
+            );
+        }
+
+        else
+        {
+            percentage = Math.round
+            (
+                (poll.answerweight[i]) * 100 / (poll.pollcount + 1)
+            );
+        }
+
+        answers[i].querySelector(".percentage_bar").style.width = percentage + "%";
+        answers[i].querySelector(".percentage_value").innerText = percentage + "%";
+        
+    }
 }
-  
-displayResult(); 
-  
-  function refreshPage() {
-location.reload();
-  }
