a780007709a01230457d5cc0190ce6d2bfb4411d
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-04-10 11:41:18 +0800

    Update poll.js

--- docs/top/poll.js
+++ docs/top/poll.js
@@ -1,42 +1,52 @@
 const options = [
-  {id:"option1",text: "JavaScript", votes:0}, (id:"option2", text:"Python", votes:0}, (id: "option3", text: "Java", votes:0},
-(id:"option4", text:"(++", votes:0},
-v function submitVote
-く
-const selectedOption = document-querySelector ('input [name="poll"]: checked*);
+  {id:"option1",text: "JavaScript", votes:0}, 
+  {id:"option2", text:"Python", votes:0}, 
+  {id: "option3", text: "Java", votes:0},
+  {id:"option4", text:"(++", votes:0},
+
+  function submitVote(){
+const selectedOption = document.querySelector('input[name="poll"]:checked');
 // console. log(selectedOption.value);
-if(IselectedOption)(
+if(!selectedOption){
+  
 alert("Please select an optin.");
 return;
-く
-｝
-const optionid = selectedOption.value;
-const selectedOptionobj = options. find (option)=> option.id === optionid);
-// console. log(selectedOptionObj):
-if(selectedOptionobj)
-selectedOption0bj.votes++;
-console. log(selectedOptionobj);
-displayResult);
-v function displayResult
-const result = document.getElementById("result' )B
-result. innerHTML = ™$
-✓
-options. forEach ((option)=>
-const percentage = ((option.votes/ getTotalVotes() * 100).toFixed(2) || 0;
-const barwidth = percentage › 0 ? percentage + "*" : "0%";
-const optionResult = document.createElement ("div");
-optionResult.className = "option-result";
-optionResult.innerHTML =
-‹span class = "option-text">${option.text}</span>
-‹div class = "bar-container">
-‹div class = "bar" style="width: {barWidth);"*/div>
-</div>
-‹span class = "percentage">${percentage}%</span>
-result.appendChild(optionResult);
-3)8
-v function getTotalVotesE
-return options reduce(total,option)=> total + option.votes,0);
 }
-displayResultO; function refreshPage) €
+
+const optionId = selectedOption.value;
+const selectedOptionObj = options.find((option)=> option.id === optionid);
+// console. log(selectedOptionObj):
+if(selectedOptionObj){
+  selectedOptionObj.votes++;
+  console.log(selectedOptionObj);
+  displayResult();
+  
+function displayResult(){
+const result = document.getElementById('result');
+result.innerHTML = "";
+
+options.forEach((option)=>{
+  const percentage = ((option.votes/ getTotalVotes()) * 100).toFixed(2) || 0;
+  const barWidth = percentage > 0 ? percentage + "%" : "0%";
+  
+  const optionResult = document.createElement("div");
+  optionResult.className = "option-result";
+  optionResult.innerHTML = '
+  <span class = "option-text">${option.text}</span>
+  <div class = "bar-container">
+  <div class = "bar" style="width: ${barWidth};"></div>
+  </div>
+  <span class = "percentage">${percentage}%</span>
+  ;
+  result.appendChild(optionResult);
+});
+
+function getTotalVotes(){
+  return options.reduce((total,option)=> total + option.votes,0);
+}
+  
+displayResult(); 
+  
+  function refreshPage() {
 location.reload();
-｝
+  }
