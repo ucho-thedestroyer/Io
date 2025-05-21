8cd9d1bad6399760be5d506a8a8266526b1f830b
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-05-21 16:45:52 +0800

    Update tv.js

--- docs/tv/tv.js
+++ docs/tv/tv.js
@@ -1,1 +1,13 @@
 
+  const video = document.getElementById('myVideo');
+  const button = document.getElementById('muteToggleBtn');
+
+  button.addEventListener('click', () => {
+    video.muted = !video.muted;
+    button.textContent = video.muted ? 'Unmute' : 'Mute';
+  });
+
+  const slider = document.getElementById('volumeSlider');
+  slider.addEventListener('input', () => {
+    video.volume = slider.value;
+  });
