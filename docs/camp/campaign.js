cb850932bea16a977b883ee6646f9ea0d2710807
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-05-26 14:07:35 +0800

    Update campaign.js

--- docs/camp/campaign.js
+++ docs/camp/campaign.js
@@ -1,34 +1,3 @@
-/*
- * Star Wars opening crawl from 1977
- * 
- * I freaking love Star Wars, but could not find
- * a web version of the original opening crawl from 1977.
- * So I created this one.
- *
- * I wrote an article where I explain how this works:
- * https://nerddis.co/star-wars-opening-crawl-from-1977
- * 
- * Watch the Start Wars opening crawl on YouTube.
- * https://www.youtube.com/watch?v=7jK-jZo6xjY
- * 
- * Stuff I used:
- * - CSS (animation, transform)
- * - HTML audio (the opening theme)
- * - SVG (the Star Wars logo from wikimedia.org)
- *   http://commons.wikimedia.org/wiki/File:Star_Wars_Logo.svg
- * - JavaScript (to sync the animation/audio)
- *
- * Thanks to Craig Buckler for his amazing article 
- * which helped me to create this remake of the Star Wars opening crawl. 
- * http://www.sitepoint.com/css3-starwars-scrolling-text/ 
- *
- * Sound copyright by The Walt Disney Company.
- * 
- *
- * 2013 by Tim Pietrusky
- * nerddis.co
- * 
- */
 StarWars = (function() {
   
     /* 
@@ -40,6 +9,9 @@ StarWars = (function() {
       
       // Audio to play the opening crawl
       this.audio = this.el.find('audio').get(0);
+
+      // 🔽 Set playback speed to 80% of normal
+      this.audio.playbackRate = 0.8;
       
       // Start the animation
       this.start = this.el.find('.start');
