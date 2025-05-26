ccbe2df52e1eaad5a268c4b680eb6b2900ee9364
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-05-26 15:13:11 +0800

    Update campaign.js

--- docs/camp/campaign.js
+++ docs/camp/campaign.js
@@ -1,86 +1,69 @@
-StarWars = (function() {
-
+StarWars = (function () {
   function StarWars(args) {
     this.el = $(args.el);
     this.audio = this.el.find('audio').get(0);
 
-    // Setup Web Audio API
     this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
     this.sourceNode = this.audioContext.createMediaElementSource(this.audio);
 
-    // Create gain nodes for mixing
     this.songGain = this.audioContext.createGain();
     this.noiseGain = this.audioContext.createGain();
-    this.noiseGain.gain.value = 0.1; // White noise volume
+    this.noiseGain.gain.value = 0.05; // Adjust for radio effect
 
-    // Bandpass filter for broken radio effect
     this.filter = this.audioContext.createBiquadFilter();
     this.filter.type = 'bandpass';
     this.filter.frequency.value = 1000;
-    this.filter.Q.value = 1;
+    this.filter.Q.value = 0.7;
 
-    // Create white noise source
-    this.noiseSource = this.createWhiteNoise();
+    // Create white noise buffer
+    this.noiseBuffer = this.audioContext.createBuffer(1, 2 * this.audioContext.sampleRate, this.audioContext.sampleRate);
+    let data = this.noiseBuffer.getChannelData(0);
+    for (let i = 0; i < data.length; i++) {
+      data[i] = (Math.random() * 2 - 1) * 0.4;
+    }
 
-    // Connect nodes:
-    // Song -> filter -> songGain
+    this.noiseSource = null;
+    this.playNoise = () => {
+      this.noiseSource = this.audioContext.createBufferSource();
+      this.noiseSource.buffer = this.noiseBuffer;
+      this.noiseSource.loop = true;
+      this.noiseSource.connect(this.noiseGain);
+      this.noiseSource.start(0);
+    };
+
+    // Connect everything
     this.sourceNode.connect(this.filter);
     this.filter.connect(this.songGain);
 
-    // Noise -> noiseGain
-    this.noiseSource.connect(this.noiseGain);
+    this.songGain.connect(this.audioContext.destination);
+    this.noiseGain.connect(this.audioContext.destination);
 
-    // Merge song and noise
-    this.merger = this.audioContext.createGain();
-    this.songGain.connect(this.merger);
-    this.noiseGain.connect(this.merger);
-
-    // Connect to output
-    this.merger.connect(this.audioContext.destination);
-
-    // Playback speed 80%
     this.audio.playbackRate = 0.8;
-
-    this.started = false;
 
     this.startBtn = this.el.find('.start');
     this.animation = this.el.find('.animation');
 
     this.reset();
 
-    this.startBtn.bind('click', $.proxy(function() {
+    this.startBtn.on('click touchstart', (e) => {
+      e.preventDefault();
       this.startBtn.hide();
 
-      // Resume AudioContext on user interaction
       this.audioContext.resume().then(() => {
+        this.playNoise();
         this.audio.play();
         this.el.append(this.animation);
       });
+    });
 
-      this.started = true;
-    }, this));
-
-    $(this.audio).bind('ended', $.proxy(function() {
+    $(this.audio).on('ended', () => {
       this.audio.currentTime = 0;
+      if (this.noiseSource) this.noiseSource.stop();
       this.reset();
-    }, this));
+    });
   }
 
-  StarWars.prototype.createWhiteNoise = function() {
-    const bufferSize = 2 * this.audioContext.sampleRate;
-    const buffer = this.audioContext.createBuffer(1, bufferSize, this.audioContext.sampleRate);
-    const data = buffer.getChannelData(0);
-    for (let i = 0; i < bufferSize; i++) {
-      data[i] = (Math.random() * 2 - 1) * 0.3;
-    }
-    const whiteNoise = this.audioContext.createBufferSource();
-    whiteNoise.buffer = buffer;
-    whiteNoise.loop = true;
-    whiteNoise.start(0);
-    return whiteNoise;
-  };
-
-  StarWars.prototype.reset = function() {
+  StarWars.prototype.reset = function () {
     this.startBtn.show();
     this.cloned = this.animation.clone(true);
     this.animation.remove();
@@ -88,9 +71,8 @@ StarWars = (function() {
   };
 
   return StarWars;
-
 })();
 
-const intro = new StarWars({
-  el : '.starwars'
+$(document).ready(function () {
+  new StarWars({ el: '.starwars' });
 });
