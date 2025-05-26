a2849c602ef2e5551dca75efdd0eace95abb50b8
ucho-thedestroyer <204959246+ucho-thedestroyer@users.noreply.github.com>
2025-05-26 16:33:34 +0800

    Update campaign.js

--- docs/camp/campaign.js
+++ docs/camp/campaign.js
@@ -1,78 +1,54 @@
-StarWars = (function () {
-  function StarWars(args) {
-    this.el = $(args.el);
-    this.audio = this.el.find('audio').get(0);
+StarWars = (function() {
+  
+    /* 
+     * Constructor
+     */
+    function StarWars(args) {
+      // Context wrapper
+      this.el = $(args.el);
+      
+      // Audio to play the opening crawl
+      this.audio = this.el.find('audio').get(0);
 
-    this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    this.sourceNode = this.audioContext.createMediaElementSource(this.audio);
-
-    this.songGain = this.audioContext.createGain();
-    this.noiseGain = this.audioContext.createGain();
-    this.noiseGain.gain.value = 0.05; // Adjust for radio effect
-
-    this.filter = this.audioContext.createBiquadFilter();
-    this.filter.type = 'bandpass';
-    this.filter.frequency.value = 1000;
-    this.filter.Q.value = 0.7;
-
-    // Create white noise buffer
-    this.noiseBuffer = this.audioContext.createBuffer(1, 2 * this.audioContext.sampleRate, this.audioContext.sampleRate);
-    let data = this.noiseBuffer.getChannelData(0);
-    for (let i = 0; i < data.length; i++) {
-      data[i] = (Math.random() * 2 - 1) * 0.4;
-    }
-
-    this.noiseSource = null;
-    this.playNoise = () => {
-      this.noiseSource = this.audioContext.createBufferSource();
-      this.noiseSource.buffer = this.noiseBuffer;
-      this.noiseSource.loop = true;
-      this.noiseSource.connect(this.noiseGain);
-      this.noiseSource.start(0);
-    };
-
-    // Connect everything
-    this.sourceNode.connect(this.filter);
-    this.filter.connect(this.songGain);
-
-    this.songGain.connect(this.audioContext.destination);
-    this.noiseGain.connect(this.audioContext.destination);
-
-    this.audio.playbackRate = 0.8;
-
-    this.startBtn = this.el.find('.start');
-    this.animation = this.el.find('.animation');
-
-    this.reset();
-
-    this.startBtn.on('click touchstart', (e) => {
-      e.preventDefault();
-      this.startBtn.hide();
-
-      this.audioContext.resume().then(() => {
-        this.playNoise();
+      // 🔽 Set playback speed to 80% of normal
+      this.audio.playbackRate = 0.8;
+      
+      // Start the animation
+      this.start = this.el.find('.start');
+      
+      // The animation wrapper
+      this.animation = this.el.find('.animation');
+      
+      // Remove animation and shows the start screen
+      this.reset();
+  
+      // Start the animation on click
+      this.start.bind('click', $.proxy(function() {
+        this.start.hide();
         this.audio.play();
         this.el.append(this.animation);
-      });
-    });
-
-    $(this.audio).on('ended', () => {
-      this.audio.currentTime = 0;
-      if (this.noiseSource) this.noiseSource.stop();
-      this.reset();
-    });
-  }
-
-  StarWars.prototype.reset = function () {
-    this.startBtn.show();
-    this.cloned = this.animation.clone(true);
-    this.animation.remove();
-    this.animation = this.cloned;
-  };
-
-  return StarWars;
-})();
-
-$(document).ready(function () {
-  new StarWars({ el: '.starwars' });
-});
+      }, this));
+      
+      // Reset the animation and shows the start screen
+      $(this.audio).bind('ended', $.proxy(function() {
+        this.audio.currentTime = 0;
+        this.reset();
+      }, this));
+    }
+    
+    /*
+     * Resets the animation and shows the start screen.
+     */
+    StarWars.prototype.reset = function() {
+      this.start.show();
+      this.cloned = this.animation.clone(true);
+      this.animation.remove();
+      this.animation = this.cloned;
+    };
+  
+    return StarWars;
+  })();
+  
+  const intro = new StarWars({
+    el : '.starwars'
+  });
